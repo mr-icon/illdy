fb4bca6ec2441938b1760321b32bbda43a65bd01
mr-icon <emmanuelaustin116@gmail.com>
2022-12-29 21:50:03 -0800

    pushing back about update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <section
-      id="about"
-      className="front-page-section"
-    >
+    <section id="about" className="front-page-section">
       <div className="section-header">
         <div className="container">
           <div className="row">
@@ -32,7 +29,7 @@ function About() {
             ::before
             <div
               id="illdy_skill-2"
-              className="col-sm-offset-0 col-xs-offset-1 col-lg-4 col-lg-offset-0 widget_illdy_skill"
+              className="col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1 col-lg-4 col-lg-offset-0 widget_illdy_skill"
             >
               <div
                 className="skill"
@@ -43,7 +40,7 @@ function About() {
                   ::before
                   <div
                     className="skill-progress-bar ui-progressbar ui-corner-all ui-widget ui-widget-content"
-                    role='progressbar'
+                    role="progressbar"
                     aria-valuemin={0}
                     aria-valuemax={100}
                     aria-valuenow={60}
@@ -54,6 +51,10 @@ function About() {
                     >
                       <span
                         className="ui-progressbar-value-circle"
+                        // style="background-color: #f18b6d"
+                      ></span>
+                      <span
+                        className="ui-progressbar-value-top"
                         // style="background-color: #f18b6d"
                       >
                         "60%"
@@ -66,8 +67,107 @@ function About() {
                   </div>
                   ::after
                 </div>
-                <div>
-                  <h1>hi</h1>
+                <div className="skill-bottom" 
+                  // style="color: rgb(241, 139, 109);"
+                >
+                  <i className="fa fa-font">::before</i>
+                  <span>Typography</span>
+                </div>
+              </div>
+            </div>
+            <div
+              id="illdy_skill-3"
+              className="col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1 col-lg-4 col-lg-offset-0 widget_illdy_skill"
+            >
+              <div
+                className="skill"
+                data-skill-progress-bar-width="82"
+                data-skill-color="#f1d204"
+              >
+                <div className="skill-top clearfix">
+                  ::before
+                  <div
+                    className="skill-progress-bar ui-progressbar ui-corner-all ui-widget ui-widget-content"
+                    role="progressbar"
+                    aria-valuemin={0}
+                    aria-valuemax={100}
+                    aria-valuenow={82}
+                  >
+                    <div
+                      className="ui-progressbar-value ui-corner-left ui-widget-header"
+                      // style="width: 82%; background-color: rgb(241, 210, 4);"
+                    >
+                      <span
+                        className="ui-progressbar-value-circle"
+                        // style="background-color: #f1d204"
+                      ></span>
+                      <span
+                        className="ui-progressbar-value-top"
+                        // style="background-color: #f1d204"
+                      >
+                        "82%"
+                        <span
+                          className="ui-progressbar-value-triangle"
+                          // style="border-top-color: #f1d204; border-right-color: transparent; border-bottom-color: transparent; border-left-color: transparent;"
+                        ></span>
+                      </span>
+                    </div>
+                  </div>
+                  ::after
+                </div>
+                <div className="skill-bottom" 
+                  // style="color: rgb(241, 210, 4);"
+                >
+                  <i className="fa fa-pencil">::before</i>
+                  <span>Design</span>
+                </div>
+              </div>
+            </div>
+            <div
+              id="illdy_skill-4"
+              className="col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1 col-lg-4 col-lg-offset-0 widget_illdy_skill"
+            >
+              <div
+                className="skill"
+                data-skill-progress-bar-width="87"
+                data-skill-color="#6a4d8a"
+              >
+                <div className="skill-top clearfix">
+                  ::before
+                  <div
+                    className="skill-progress-bar ui-progressbar ui-corner-all ui-widget ui-widget-content"
+                    role="progressbar"
+                    aria-valuemin={0}
+                    aria-valuemax={100}
+                    aria-valuenow={87}
+                  >
+                    <div
+                      className="ui-progressbar-value ui-corner-left ui-widget-header"
+                      // style="width: 87%; background-color: rgb(106, 77, 138);"
+                    >
+                      <span
+                        className="ui-progressbar-value-circle"
+                        // style="background-color: #6a4d8a"
+                      ></span>
+                      <span
+                        className="ui-progressbar-value-top"
+                        // style="background-color: #6a4d8a"
+                      >
+                        "87%"
+                        <span
+                          className="ui-progressbar-value-triangle"
+                          // style="border-top-color: #6a4d8a; border-right-color: transparent; border-bottom-color: transparent; border-left-color: transparent;"
+                        ></span>
+                      </span>
+                    </div>
+                  </div>
+                  ::after
+                </div>
+                <div className="skill-bottom" 
+                  // style="color: rgb(106, 77, 138);"
+                >
+                  <i className="fa fa-code">::before</i>
+                  <span>Development</span>
                 </div>
               </div>
             </div>
